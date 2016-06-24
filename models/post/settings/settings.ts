6d1b2eb71292638806ea26d7f91c888bfd60f541
Alex Hicks <aldahick@iupui.edu>
2016-06-24 09:11:48 -0400

    Finish implementing /edit/settings

--- models/post/settings/settings.ts
+++ models/post/settings/settings.ts
@@ -1,9 +1,10 @@
 import * as eta from "eta-lib";
 import * as express from "express";
+import * as querystring from "querystring";
 
 export class Model implements eta.Model {
     public render(req : express.Request, res : express.Response, callback : (env : {[key : string] : any}) => void) : void {
-        if ((!eta.params.test(req.body, ["page", "name", "type"])) || (!req.body.value && req.body.type != "boolean")) {
+        if ((!eta.params.test(req.body, ["page", "name", "type"])) || ((!req.body.value && req.body.value !== "") && req.body.type != "boolean")) {
             callback({errcode: eta.http.InvalidParameters});
             return;
         }
@@ -15,7 +16,7 @@ export class Model implements eta.Model {
             eta.logger.trace("Setting set: " + success);
             eta.setting.init();
             if (req.body.shouldRedirect) {
-                res.redirect("../edit/settings");
+                res.redirect("../edit/settings?lastPage=" + querystring.escape(req.body.page));
             } else {
                 res.send(success.toString());
             }
--- static/js/edit/settings.ts
+++ static/js/edit/settings.ts
@@ -1,36 +1,85 @@
 /// <reference path="../autoload.ts"/>
 
+/// <amd-dependency path="jquery-ui"/>
+
 export module settings {
-    $(document).ready(function() {
-        $("#new-page-submit").on("click", function() {
-            let pageName : string = $("#new-page-name").val();
-            if (pageName == "") {
+
+    // used and modified from http://stackoverflow.com/a/5158301
+    function getParameterByName(name : string) : string {
+        var match = RegExp("[?&]" + name + "=([^&]*)").exec(window.location.search);
+        return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+    }
+
+    function getInputSubmitHandler(callback : () => void) : (evt : JQueryEventObject) => void {
+        return function(evt : JQueryEventObject) {
+            if (evt.which == 13) { // pressed enter
+                callback.call(this);
+            }
+        };
+    }
+
+    function onAddSetting() {
+        let page : string = this.getAttribute("data-page");
+        let params : {[key : string] : string} = {
+            "value": "",
+            "page": page
+        };
+        $("[data-action='add-setting'][data-page='" + page + "']").each(function() {
+            if (this.tagName.toLowerCase() == "button") {
                 return;
             }
-            if (!pageName.startsWith("/")) {
-                pageName = "/" + pageName;
-            }
-            let div : JQuery = $($("div.page-section")[0]).clone();
-            div.find("h3").html(pageName);
-            div.find("tbody").attr("data-page", pageName);
-            div.find("tbody").html(""); // clearing any rows
-            $("#pages").append(div);
+            params[this.getAttribute("data-param")] = this.value;
         });
-        $("button[data-action='add-setting']").on("click", function() {
-            let page : string = this.getAttribute("data-page");
-            let params : {[key : string] : string} = {
-                "value": "",
-                "page": page
-            };
-            $("[data-action='add-setting'][data-page='" + page + "']").each(function() {
-                if (this.tagName.toLowerCase() == "button") {
+        console.log(params);
+        $.post("../post/settings", params, function(data) {
+            // location.reload();
+        });
+    }
+
+    function onNewPage() {
+        let pageName : string = $("#new-page-name").val();
+        if (pageName == "") {
+            return;
+        }
+        if (!pageName.startsWith("/")) {
+            pageName = "/" + pageName;
+        }
+        let div : JQuery = $($("div.page-section")[0]).clone();
+        div.find("h3.page-title").html(pageName);
+        div.find("tbody").attr("data-page", pageName);
+        div.find("tbody").html(""); // clearing any rows
+        div.find("button[data-action='add-setting']").on("click", onAddSetting);
+        div.find("input[data-action='add-setting'][data-param='name']").on("keydown", getInputSubmitHandler(onAddSetting));
+        div.find(".new-setting-form").children().each(function(index : number, element : HTMLElement) {
+            console.log(element);
+            element.setAttribute("data-page", pageName);
+        });
+        $("#pages").append(div);
+    }
+
+    $(document).ready(function() {
+        $("#new-page-name").on("keydown", getInputSubmitHandler(onNewPage));
+        $("#new-page-submit").on("click", onNewPage);
+
+        $("input[data-action='add-setting'][data-param='name']").on("keydown", getInputSubmitHandler(onAddSetting));
+        $("button[data-action='add-setting']").on("click", onAddSetting);
+
+        let active : boolean | number = false;
+        let lastPage : string = getParameterByName("lastPage");
+        if (lastPage) {
+            $("div.page-section").each(function(index : number, element : HTMLElement) {
+                if (element.children[0].innerHTML == lastPage) {
+                    active = index;
                     return;
                 }
-                params[this.getAttribute("data-param")] = this.value;
             });
-            $.post("../post/settings", params, function(data) {
-                location.reload();
-            });
+        }
+
+        $("#pages").accordion({
+            "active": active,
+            "header": "> div.page-section > h3.page-title",
+            "collapsible": true,
+            "heightStyle": "content"
         });
     });
 }
