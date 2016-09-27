42651223e9dc8344ac87376584e272107e0aa1b7
Alex Hicks <aldahick@iupui.edu>
2016-09-27 12:22:04 -0400

    Format all pre-existing code

--- models/edit/settings/settings.ts
+++ models/edit/settings/settings.ts
@@ -2,7 +2,7 @@ import * as eta from "eta-lib";
 import * as express from "express";
 
 export class Model implements eta.Model {
-    public render(req : express.Request, res : express.Response, callback : (env : {[key : string] : any}) => void) : void {
-        callback({pages: eta.setting.settings});
+    public render(req: express.Request, res: express.Response, callback: (env: { [key: string]: any }) => void): void {
+        callback({ pages: eta.setting.settings });
     }
 }
--- models/post/settings/settings.ts
+++ models/post/settings/settings.ts
@@ -3,16 +3,16 @@ import * as express from "express";
 import * as querystring from "querystring";
 
 export class Model implements eta.Model {
-    public render(req : express.Request, res : express.Response, callback : (env : {[key : string] : any}) => void) : void {
+    public render(req: express.Request, res: express.Response, callback: (env: { [key: string]: any }) => void): void {
         if ((!eta.params.test(req.body, ["page", "name", "type"])) || ((!req.body.value && req.body.value !== "") && req.body.type != "boolean")) {
-            callback({errcode: eta.http.InvalidParameters});
+            callback({ errcode: eta.http.InvalidParameters });
             return;
         }
         if (req.body.type == "boolean") {
             req.body.value = !!req.body.value;
         }
         eta.logger.json(req.body);
-        eta.setting.set(req.body.page, req.body.name, req.body.value, req.body.type, (success : boolean) => {
+        eta.setting.set(req.body.page, req.body.name, req.body.value, req.body.type, (success: boolean) => {
             eta.logger.trace("Setting set: " + success);
             eta.setting.init();
             if (req.body.shouldRedirect) {
